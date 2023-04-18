605c285a1933b9b7e45a9984220c3cd83c011a11
Jehizkia <Jehizkia@users.noreply.github.com>
2023-04-18 21:00:37 +0200

    Added translations

--- resources/lang/es/modal.php
+++ resources/lang/es/modal.php
@@ -2,6 +2,7 @@
 
 return [
     'locked_notice' => 'Esta página ha sido bloqueada porque otro usuario la está editando actualmente.',
+    'locked_notice_user' => 'está editando actualmente esta página, por lo que está bloqueada para editar en este momento.',
     'unlock_button' => 'Desbloquear página',
     'return_button' => 'Regresar',
 ];
