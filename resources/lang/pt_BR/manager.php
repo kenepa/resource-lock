43178de77a0984e25e563268191f1b4d0baaa37c
Jackson Belloli <30443980+JacksonBelloli@users.noreply.github.com>
2025-06-24 09:56:51 -0300

    Adding portuguese translation (#61)

---
+++ resources/lang/pt_BR/manager.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'active' => 'Ativo',
+    'expired' => 'Expirado',
+    'unlock' => 'Desbloquear',
+    'unlocked' => 'Recursos desbloqueados',
+    'unlocked_selected' => 'Recursos selecionados desbloqueados',
+    'unlock_all' => 'Desbloquear todos os recursos',
+];
