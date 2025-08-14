e42522249e5c8cf7c460f339f6a274db0f487179
Jehizkia <Jehizkia@users.noreply.github.com>
2025-08-14 13:27:44 +0200

    minor styling fixes

--- resources/views/components/resource-lock-observer.blade.php
+++ resources/views/components/resource-lock-observer.blade.php
@@ -68,7 +68,7 @@
         :closeByEscaping="false"
     >
         <div x-data="{ resourceLockOwner: null}"  @open-modal.window="(event) => { resourceLockOwner = event.detail.resourceLockOwner}">
-            <div class="flex justify-center ">
+            <div style="display:flex; justify-content:center; margin-bottom: 0.5rem">
                 <x-filament::icon-button icon="heroicon-s-lock-closed" size="lg" tabindex="-1"/>
             </div>
             <p x-show="resourceLockOwner" class="text-center pt-2">
@@ -85,16 +85,16 @@
             Livewire.dispatch('resourceLockObserver::unlock')
             Livewire.dispatch('close-modal', {id: 'resourceIsLockedNotice'})
         }
-        }" @open-modal.window="(event) => { url = event.detail.returnUrl}" class="flex flex-col justify-center space-y-2">
+        }" @open-modal.window="(event) => { url = event.detail.returnUrl}" style="display:flex; flex-direction:column; justify-content:center;">
 
             @if ($isAllowedToUnlock)
-                <button x-on:click="unlock()" style="--c-400:var(--danger-400);--c-500:var(--danger-500);--c-600:var(--danger-600);" class="fi-btn fi-btn-size-md relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus:ring-2 disabled:pointer-events-none disabled:opacity-70 rounded-lg fi-btn-color-danger gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50 fi-ac-btn-action" tabindex="-1">
+                <button x-on:click="unlock()" style="margin-bottom: 0.5rem" class="fi-color fi-color-primary fi-bg-color-600 hover:fi-bg-color-500 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-0 hover:fi-text-color-0 dark:fi-text-color-0 dark:hover:fi-text-color-0 fi-btn fi-size-md" tabindex="-1">
                     {{ __('resource-lock::modal.unlock_button') }}
                 </button>
             @endif
 
-            <a style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
-               class="fi-btn fi-btn-size-md relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus:ring-2 disabled:pointer-events-none disabled:opacity-70 rounded-lg fi-btn-color-primary gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50 fi-ac-btn-action"
+            <a
+               class="fi-btn fi-size-md"
                :href="url" tabindex="-1">
                 <span>
                     {{ __('resource-lock::modal.return_button') }}
